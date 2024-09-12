200a41ef9f372c73faf63fcc7e8a432124fdcbe3
chltpgns987 <tpgns4259@gmail.com>
2024-09-12 17:36:03 +0900

    hello world

--- hello_wolrd.py
+++ hello_wolrd.py
@@ -1,7 +1,9 @@
-Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def print_hello():
-...     print("Hello World!!")
-... 
-...     
->>> if __name__ == '__main__':
+def print_hello():
+    print("Hellow World!!")
+
+def print_KIT():
+    print("Kumoh National Institute of Technology.")
+
+if __name__ == '__main__':
+    print_KIT()
+    print_hello()
