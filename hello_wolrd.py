ba89faa95de4d871b7e7c556de30a9da1d4f0a1e
chltpgns987 <tpgns4259@gmail.com>
2024-09-13 10:18:15 +0900

    kit

--- hello_wolrd.py
+++
@@ -1,9 +0,0 @@
-def print_hello():
-    print("Hellow World!!")
-
-def print_KIT():
-    print("Kumoh National Institute of Technology.")
-
-if __name__ == '__main__':
-    print_KIT()
-    print_hello()
